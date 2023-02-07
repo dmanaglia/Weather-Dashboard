b70e9bc3fd8b7dc5ab9474ccea499954025b3341
dmanaglia <ddsmm.managlia@gmail.com>
2023-02-07 00:19:19 -0600

    fixed bug with autocomplete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var favList = JSON.parse(localStorage.getItem("favCities")) || [];
 var openMapsApiK = "c4569ddf88609f987ce3eae232bd2c96";
-var access_token = "pk.eyJ1IjoiZG1hbmFnbGlhIiwiYSI6ImNsZHRtMzE0ZjFxdDAzcHA2MWh2ZjRwOG4ifQ.a6vf0lK-YsPOBxknrJMJSA";
+var access_token = "pk.eyJ1IjoiZG1hbmFnbGlhIiwiYSI6ImNsZHFnNHBvajB2M2wzcHA2cmgxODkyOXAifQ.28GfZgfDROlt1vZRPBJvmg"
 
 function Location (cityName, lat, lon, state, country){
     this.cityName = cityName,
@@ -136,36 +136,40 @@ $("#search-txt").on("keyup", function(event){
         return response.json();
     })
     .then(function(data){
-        var availableTags = [];
-        var locations = [];
-        
-        for(var i = 0; i < data.features.length; i++){
-            var placeInfo = data.features[i].place_name.split(",");
-            var city = placeInfo[0].trim();
-            var state = placeInfo[1].trim();
-            var country = placeInfo[2].trim();
-            availableTags.push(data.features[i].place_name);
-            locations.push(new Location(city, data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0], state, country));
-        }
-        
-        $("#search-txt").autocomplete({
-            minLength: 2,
-            source: availableTags,
-            select: function(event, ui) {
-            var tagInfo = ui.item.label.split(",");
-            var city = tagInfo[0].trim();
-            var state = tagInfo[1].trim();
-            var country = tagInfo[2].trim();
-                for(var i = 0; i < locations.length; i++){
-                    if(city === locations[i].cityName && state === locations[i].state && country === locations[i].country){
-                        addSpinners();
-                        printData(locations[i]);
-                        $(this).val('');
-                        return false;
-                    }
+        if(data.features){
+            var availableTags = [];
+            var locations = [];
+            
+            for(var i = 0; i < data.features.length; i++){
+                var placeInfo = data.features[i].place_name.split(",");
+                if(placeInfo[0] && placeInfo[1] && placeInfo[2]) {
+                    var city = placeInfo[0].trim();
+                    var state = placeInfo[1].trim();
+                    var country = placeInfo[2].trim();
+                    availableTags.push(data.features[i].place_name);
+                    locations.push(new Location(city, data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0], state, country));
                 }
             }
-        });
+                
+            $("#search-txt").autocomplete({
+                minLength: 1,
+                source: availableTags,
+                select: function(event, ui) {
+                    var tagInfo = ui.item.label.split(",");
+                    var city = tagInfo[0].trim();
+                    var state = tagInfo[1].trim();
+                    var country = tagInfo[2].trim();
+                    for(var i = 0; i < locations.length; i++){
+                        if(city === locations[i].cityName && state === locations[i].state && country === locations[i].country){
+                            addSpinners();
+                            printData(locations[i]);
+                            $(this).val('');
+                            return false;
+                        }
+                    }
+                }
+            });
+        }    
     })
 });
 
